07c788ba89d5c0310ed83ad112b423d3d1e92de1
Sebastian Barrenechea <sebastian@barrenechea.cl>
2022-12-10 12:49:26 -0300

    chore: set min res, set global bgColor

--- main/index.js
+++ main/index.js
@@ -17,6 +17,8 @@ app.on("ready", async () => {
   const mainWindow = new BrowserWindow({
     width: 1000,
     height: 600,
+    minWidth: 1000,
+    minHeight: 600,
     webPreferences: {
       nodeIntegration: false,
       preload: join(__dirname, "preload.js"),
