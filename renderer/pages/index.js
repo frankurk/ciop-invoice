a8da32ebdb1d37b9bee834aaee81c44a202dda71
Francisca Caro <fran@Franciscas-Air.local>
2022-12-10 19:27:32 -0300

    index component complete

--- renderer/pages/index.js
+++ renderer/pages/index.js
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
+import ReactLoading from "react-loading";
+import { useRouter } from 'next/navigation';
 
 const LandingPage = () => {
     const [success, setSuccess] = useState(null);
     const [data, setData] = useState(null);
     const [uf, setUf] = useState(null);
+
+    const router = useRouter();
 
     const fetchRandomData = () => {
         return new Promise((resolve, reject) => {
@@ -13,32 +17,46 @@ const LandingPage = () => {
         });
     };
 
+    const updateUF = () => {
+        const date = new Date();
+
+        window.electron.general.overrideUfPrice(date, uf).then(() => {
+            router.push("/generateInvoice");
+        })
+    }
+
     useEffect(() => {
-        fetchRandomData().then((payload) => {
+        window.electron.general.getUfPrice().then((payload) => {
             setSuccess(true);
             setData(payload)
-            // redirect to dashboard
+            router.push("/generateInvoice");
         }).catch((err) => {
             setSuccess(false);
-            // ask user for manual UF input
         })
     }, []);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setUf(e.target.value)
+        setUf(e.target.value);
+        updateUF();
     }
+
+    const LoadingAnimation = (
+        <div className="w-full h-screen flex justify-center items-center">
+            <ReactLoading type="spinningBubbles" color={"#14b8a6"} height={"100px"} width={"100px"} />
+        </div>
+    );
 
     return (
         <div>
-            {success === false ? <div className="w-full h-screen flex flex-col justify-center items-center">
-                <p>Lo sentimos, hubo un error y no se pudo cargar automaticamente el valor de la UF. Por favor ingrese la UF de forma manual. </p>
-                <form onSubmit={handleSubmit}>
-                    <p>Ingrese valor UF:</p>
-                    <input type="number" onChange={(e) => setUf(e.target.value)} value={uf} />
-                    <button type="submit">Ingresar</button>
+            {success === false ? <div className="w-full h-screen flex flex-col justify-center items-center bg-zinc-100">
+                <p className="text-lg text-slate-600 mr-2 w-3/5">Lo sentimos, hubo un error y no se pudo cargar automaticamente el valor de la UF. Por favor ingrese la UF de forma manual. </p>
+                <form className="flex flex-row m-10" onSubmit={handleSubmit}>
+                    <p className="text-lg text-slate-600 mr-2 font-bold w-3/5">Valor UF:</p>
+                    <input type="number" className="border rounded-md border-slate-500 outline-none pl-2 mr-2" onChange={(e) => setUf(e.target.value)} value={uf} />
+                    <button type="submit" className="text-teal-500 font-bold outline rounded-md px-2">Ingresar</button>
                 </form>
-            </div> : null}
+            </div> : LoadingAnimation}
         </div>
     )
 }
